cf08fdaf38e2cfbe2443320efcaf24e9a442d281
Tokavindu <lkavindukeshan@gmail.com>
2024-07-08 11:38:50 +0530

    Done! Easy to read!

--- HumanAsk.java
+++
@@ -1,5 +0,0 @@
-i=1; i++ than! i wrote! I am! i(i;
-i=1; Then conect i<i Load!
-i>i add x>i; i(Let;)!
-Then accept i rule; if(x_y); //Send i wrote to video! i accept lend!
-print what they made; I ask then want!
--- Lion_i.java
+++
@@ -1,5 +0,0 @@
-Let_= i for(i++; i=3; i<3;){
-	be.option!
-}
-object-= 1<i; //Send to meet i role;
-Let it consume that area! JAVA!
